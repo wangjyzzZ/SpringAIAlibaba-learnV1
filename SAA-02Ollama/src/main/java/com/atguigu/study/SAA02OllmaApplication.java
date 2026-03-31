5bb47533ce872c473ae80d95fef1bbd1df80a221
wangjy <wangjianyang111@163.com>
2026-03-31 21:54:25 +0800

    02Ollama

---
+++ SAA-02Ollama/src/main/java/com/atguigu/study/SAA02OllmaApplication.java
@@ -0,0 +1,11 @@
+package com.atguigu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SAA02OllmaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SAA02OllmaApplication.class, args);
+    }
+}
