6e9288cf2f39a0b470d694028186bacc49753d4b
wangjy <wangjianyang111@163.com>
2026-03-31 22:48:49 +0800

    04StreamingOutput

---
+++ SAA-04StreamingOutput/src/main/java/com/atguigu/study/SAA04StreamingOutputApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SAA04StreamingOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SAA04StreamingOutputApplication.class, args);
+    }
+}
