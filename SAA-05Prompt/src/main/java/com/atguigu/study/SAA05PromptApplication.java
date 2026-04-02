fe5a3510c6b29bd7c55f2143ff2c78435a6021d7
wangjy <wangjianyang111@163.com>
2026-04-02 22:46:46 +0800

    05Prompt

---
+++ SAA-05Prompt/src/main/java/com/atguigu/study/SAA05PromptApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SAA05PromptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SAA05PromptApplication.class, args);
+    }
+}
